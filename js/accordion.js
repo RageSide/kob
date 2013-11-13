6118ebb79934cd19f1ff6a06b62db484c466c9d4
RageSide <kob2121@gmail.com>
2013-11-13 12:44:39 -0500

    Newest Updates

---
+++ js/accordion.js
@@ -0,0 +1,15 @@
+jQuery(document).ready(function ($) {
+    //Set default open/close settings
+    var divs=$('.accordion>div').hide(); //Hide/close all containers
+
+    var h2s=$('.accordion>h2').click(function () {
+        h2s.not(this).removeClass('active')
+        $(this).toggleClass('active')
+        divs.not($(this).next()).slideUp()
+        $(this).next().slideToggle()
+        return false; //Prevent the browser jump to the link anchor
+
+    });
+
+
+});
